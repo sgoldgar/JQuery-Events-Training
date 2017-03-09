d9b3af159522066e0c29e9b8a18a1a2792e43dc7
Sarah Goldgar <sgoldgar@gmail.com>
2017-03-08 22:05:57 -0600

    click exercise

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,15 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  $('#imperatives span').on('click', function(){
+    var click = console.log('click');
+    if ('click'){
+    //var content = $(this).text();
+    $('ul').append('<li>' + $(this).text());
+  };
 
-})
+});
+});
+
+
+//http://stackoverflow.com/questions/4981765/jquery-create-list-item-from-user-input-on-click
