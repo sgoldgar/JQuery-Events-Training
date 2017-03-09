4677e9e172a38497f57f56edd5531c6355e83917
Sarah Goldgar <sgoldgar@gmail.com>
2017-03-08 21:30:46 -0600

    change exercise

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
-})
+  $('#total').on('click',function(){
+    var numOne = parseInt($('#left').val());
+    var numTwo = parseInt($('#right').val());
+    var total = numOne + numTwo;
+    $('#total').val(total);
+  });
+
+});
